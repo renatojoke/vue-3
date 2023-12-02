2284d1c7f133b331138d8db12c1a181507e83425
renatojoke <xoligan1999@gmail.com>
2023-12-03 02:56:15 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -50,23 +50,23 @@ onMounted(() => {
 		
 		<section class="greeting">
 			<h2 class="title">
-				What's up, <input type="text" id="name" placeholder="Name here" v-model="name">
+				Приветствую, <input type="text" id="name" placeholder="Sergey" v-model="name">
 			</h2>
 		</section>
 
 		<section class="create-todo">
-			<h3>CREATE A TODO</h3>
+			<h3>Создать задачу</h3>
 
 			<form id="new-todo-form" @submit.prevent="addTodo">
-				<h4>What's on your todo list?</h4>
+				<h4>Какую категорию?</h4>
 				<input 
 					type="text" 
 					name="content" 
 					id="content" 
-					placeholder="e.g. make a video"
+					placeholder="Поспать 8 часов"
 					v-model="input_content" />
 				
-				<h4>Pick a category</h4>
+				<h4>Выбрать категорию</h4>
 				<div class="options">
 
 					<label>
@@ -74,10 +74,10 @@ onMounted(() => {
 							type="radio" 
 							name="category" 
 							id="category1" 
-							value="business"
+							value="Бизнес"
 							v-model="input_category" />
 						<span class="bubble business"></span>
-						<div>Business</div>
+						<div>Работа</div>
 					</label>
 
 					<label>
@@ -85,20 +85,20 @@ onMounted(() => {
 							type="radio" 
 							name="category" 
 							id="category2" 
-							value="personal"
+							value="Персонал"
 							v-model="input_category" />
 						<span class="bubble personal"></span>
-						<div>Personal</div>
+						<div>Персонал</div>
 					</label>
 
 				</div>
 
-				<input type="submit" value="Add todo" />
+				<input type="submit" value="Добавить задачу" />
 			</form>
 		</section>
 
 		<section class="todo-list">
-			<h3>TODO LIST</h3>
+			<h3>Все задачи</h3>
 			<div class="list" id="todo-list">
 
 				<div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
@@ -116,7 +116,7 @@ onMounted(() => {
 					</div>
 
 					<div class="actions">
-						<button class="delete" @click="removeTodo(todo)">Delete</button>
+						<button class="delete" @click="removeTodo(todo)">Удалить</button>
 					</div>
 				</div>
 
